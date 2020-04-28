b2ab28b2719a2a8fb8531560eb418e5ed350dbfd
Kleine-alberta <invase12@gmail.com>
2020-04-28 19:17:20 +0300

    are added tests checking success_massege with negatives methods plus tests to check login_link and login_page page + two of all tests marked as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,19 +1,18 @@
-from selenium.common.exceptions import NoAlertPresentException
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 import time
 import pytest
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+@pytest.mark.parametrize('link', [#"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  #pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     link = f"{link}"
@@ -21,7 +20,46 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.click_button_add_to_basket()
     page.solve_quiz_and_get_code()
+    #time.sleep(30)
+    page.should_be_add_to_basket()
+
+@pytest.mark.xfail     
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/stand-on-zanzibar_90/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_button_add_to_basket()
     time.sleep(3)
-    z = browser.find_element_by_css_selector("div.alertinner").get_attribute("innerText")
-    q = "Coders at Work has been added to your basket."
-    assert z == q, "wrong link"
+    page.should_not_be_success_message()
+    
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/stand-on-zanzibar_90/"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(3)
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail    
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/stand-on-zanzibar_90/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_button_add_to_basket()
+    time.sleep(3)
+    page.should_be_success_message()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    
+    
+    
