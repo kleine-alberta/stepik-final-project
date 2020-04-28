57d32d74f818d7a14709d58d0e2199d9390d4892
Kleine-alberta <invase12@gmail.com>
2020-04-28 19:08:28 +0300

    are added negative cheks, + replaced login_link and login_page checks from main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 import math
 import time
 
@@ -7,23 +11,47 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-    
-    def open(self):
-        self.browser.get(self.url)
-        
+            
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+        
+    def is_disappeared(self, how, what, timeout=4):   #ожидаем, что элемент исчезает
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+            
+        return True
 
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
-        return True    
+        return True
         
-    def solve_quiz_and_get_code(self):
+    def is_not_element_present(self, how, what, timeout=4):  #проверяет, что элемент не появляется на странице в течение заданного времени
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+            
+        return False
+    
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+        
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        
+    def solve_quiz_and_get_code(self):  #для решения уравнения для получение кода и перехода нужную страницу
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
@@ -37,3 +65,11 @@ class BasePage():
             time.sleep(30)
         except NoAlertPresentException:
             print("No second alert presented")
+            
+
+    
+    
+
+     
+  
+    
