610141b904999e27eb5aa32205dbb36c3635621a
Kleine-alberta <invase12@gmail.com>
2020-04-28 19:09:53 +0300

    is added new BasePageClass

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -10,3 +13,8 @@ class LoginPageLocators():
     
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR,"div.alertinner > strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.row p.price_color")
+    
+
