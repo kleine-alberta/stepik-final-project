22bafd8521fba339eea65d05acfd30c6433f1694
Kleine-alberta <invase12@gmail.com>
2020-04-28 19:12:39 +0300

    are added success_masseg_methods and method_able_add_to_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
@@ -7,8 +7,23 @@ class ProductPage(BasePage):
         button_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         button_basket.click() 
         
-    def should_be_button_add_to_basket(self):
-        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Button add to basket is not found"  
+    def should_be_add_to_basket(self):
+        Message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        Product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert Message == Product_name, "wrong link"
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should not be"
+       
+    def should_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but not disappeared"
+       
+    
+        
+       
+    
 
         
         
