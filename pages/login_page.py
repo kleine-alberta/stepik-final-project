d91d2498b646af16859f3d9e10d17f1ece7477df
Kleine-alberta <invase12@gmail.com>
2020-05-12 15:00:06 +0300

    added new method which registers new users

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,3 +18,14 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not found"
+        
+    def register_new_user(self, email, password):
+        get_email = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        get_email.send_keys(email)
+        get_password = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD)
+        get_password.send_keys(password)
+        confirmed = self.browser.find_element(*LoginPageLocators.CONFIRM_PASS)
+        confirmed.send_keys(password)
+        button = self.browser.find_element_by_css_selector('[name="registration_submit"]')
+        button.click()
+        assert True, "user  was not autorised"
